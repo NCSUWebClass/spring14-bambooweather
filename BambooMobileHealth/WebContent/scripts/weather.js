4dfa97230964b49b0a1191b099e20be1e69f3825
Jesse Couch <jccouch@ncsu.edu>
2014-04-27 19:47:32 -0400

    JSON conversion

--- BambooMobileHealth/WebContent/scripts/weather.js
+++ BambooMobileHealth/WebContent/scripts/weather.js
@@ -1,9 +1,24 @@
 function getWeather(gps){
   data = "";
-  var url_str = 'http://api.wunderground.com/auto/wui/geo/WXCurrentObXML/index.xml?query=' + gps;
+  var url_str = 'http://api.wunderground.com/auto/wui/geo/WXCurrentObXML/index.xml?query='+ gps;
 };
+
 
 function parse(xml) {
 	alert("parsed");
+	
 }
 
+//var url = 'http://api.wunderground.com/api/77a956bf764dc788/history_20140421/q/NC/Raleigh.json';
+var url = 'http://api.wunderground.com/api/77a956bf764dc788/geolookup/conditions/q/NC/Raleigh.json';
+jQuery(document).ready(function($) {
+	  $.ajax({
+	  url : url,
+	  dataType : "jsonp",
+	  success : function(parsed_json) {
+	  var location = parsed_json['location']['city'];
+	  var temp_f = parsed_json['current_observation']['temp_f'];
+	  alert("Current temperature in " + location + " is: " + temp_f);
+	  }
+	  });
+});
